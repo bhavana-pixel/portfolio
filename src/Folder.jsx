9a6ef94b2679e4e32095d5520baf00f345897454
bhavana-pixel <bhavana0412@hotmail.com>
2025-12-26 13:18:37 -0500

    Created folder and tabs.

--- src/Folder.jsx
+++ src/Folder.jsx
@@ -3,27 +3,39 @@ import "./Folder.css";
 
 export default function Folder() {
     const [activeTab, setActiveTab] = useState("About");
+    const [open, setOpen] = useState(false);
+
     const tabs = ["About", "Resume", "Projects", "Experience"];
 
     return (
-        <div className="folder-container">
+        <div 
+            className={`folder-container ${open ? "open" : ""}`}
+            onClick = {() => !open && setOpen(true)}
+        >
             <div className = "tabs">
                 {tabs.map((tab) => (
-                    <button key = {tab} className = {'tab $(activeTab === tab ? "active" : ""}'}
-                    onclick = {() => setActiveTab(tab)}
+                    <button key = {tab} className = {`tab ${activeTab === tab ? "active" : ""}`}
+                    onClick = {(e) => {
+                        e.stopPropagation();
+                        setActiveTab(tab);
+                        setOpen(true);
+                    }}
                     >
                         {tab}
                     </button>
                 ))}
             </div>
-
-            <div className = "tab-content">
+            
+            {open && (
+            <div className = "tab-content" style={{ display: open ? "block" : "none" }}>
                 {activeTab === "About" && <p> Welcome! My name is Bhavana! </p>}
                 {activeTab === "Resume" && <p> Resume content here...</p>}
                 {activeTab === "Projects" && <p> Here are my projects! </p>}
-                {activeTab === "Experience" && <p> Internships and work experience here ...</p>}
-
+                {activeTab === "Experience" && (
+                    <p> Internships and work experience here ...</p>
+                )}
             </div>
+         )}
         </div>
     );
 }

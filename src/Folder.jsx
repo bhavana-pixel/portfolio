93eb0ba0780e91ea3b928bb74dfcc5aa2a374a9a
bhavana-pixel <bhavana0412@hotmail.com>
2026-01-05 23:06:36 -0500

    Added star to bottom of about me.

--- src/Folder.jsx
+++ src/Folder.jsx
@@ -72,6 +72,7 @@ export default function Folder() {
                     “For the things we have to learn before we can do them, we learn by doing them.”
                   </p>
                   <p className="author">— Aristotle</p>
+                  <p className="star"> ★</p>
                 </div>
               </div>
             )}

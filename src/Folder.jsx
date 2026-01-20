2674e475906c8c87d4e69644157572412136a021
bhavana-pixel <bhavana0412@hotmail.com>
2026-01-19 22:23:40 -0500

    Need to move project 3 image more to the right.

--- src/Folder.jsx
+++ src/Folder.jsx
@@ -227,7 +227,7 @@ export default function Folder() {
         <img
           src="/goodoledays.jpg"
           alt="Good Ole' Days project"
-          className="project-image3"
+          className="project-image"
         />
         <a
           href="https://github.com/steelhacks-2025-devs/main"
@@ -238,7 +238,7 @@ export default function Folder() {
           <img
             src="/github-logo.png"
             alt="GitHub Repository"
-            className="github-logo-3"
+            className="github-logo"
           />
         </a>
       </div>

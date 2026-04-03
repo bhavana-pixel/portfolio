a6fd3af983cf5cdc35100b6721b486c529fa45f9
bhavana-pixel <bhavana0412@hotmail.com>
2026-04-02 21:56:18 -0400

    Added whitespace.

--- src/Folder.jsx
+++ src/Folder.jsx
@@ -125,6 +125,7 @@ export default function Folder() {
             {activeTab === "Resume" && (
               <div className="resume-layout">
                 <h3>Resume</h3>
+                
                 {/* Changed pic URL to deploy */}
                 <img
                   src={`${import.meta.env.BASE_URL}resume.jpg`}

cc70be0d8ee60c341b403b1d4e6d2aefa4e17de1
bhavana-pixel <bhavana0412@hotmail.com>
2026-05-12 12:36:02 -0400

    Formatted app.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ export default function App() {
   // Variables
   const [loading, setLoading] = useState(true);    // terminal visible
   const [showPortfolio, setShowPortfolio] = useState(false); // portfolio mounted
-  
+
   const handleFinish = () => {
     // Start fading out terminal
     setLoading(false);
@@ -25,41 +25,41 @@ export default function App() {
     <>
       {showPortfolio && (
         <div className={`page fade-in-overlap ${!loading ? "visible" : ""}`}>
-           {/* 🔗 Social links */}
-    <div className="social-links">
-      
-      {/* Github URL */}
-      <a
-        href="https://github.com/bhavana-pixel"
-        target="_blank"
-        rel="noopener noreferrer"
-        aria-label="GitHub"
-      >
-        <img src={`${import.meta.env.BASE_URL}github-logo.png`} alt="GitHub" />
-      </a>
-      
-      {/* Linkedin URL */}
-      <a
-        href="https://www.linkedin.com/in/bhavanadakshinamoorthy/"
-        target="_blank"
-        rel="noopener noreferrer"
-        aria-label="LinkedIn"
-      >
-        <img src={`${import.meta.env.BASE_URL}linkedin.jpg`} alt="LinkedIn" />
-      </a>
+          {/* 🔗 Social links */}
+          <div className="social-links">
 
-      {/* Email link */}
-      <a 
-        href = "mailto:bdakshinamoorthy4@gmail.com"
-        aria-label="Mail"
-      >
-        <img src = {`${import.meta.env.BASE_URL}mail.png`}  alt="Email"/>
-      </a>
+            {/* Github URL */}
+            <a
+              href="https://github.com/bhavana-pixel"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="GitHub"
+            >
+              <img src={`${import.meta.env.BASE_URL}github-logo.png`} alt="GitHub" />
+            </a>
 
-    </div>
+            {/* Linkedin URL */}
+            <a
+              href="https://www.linkedin.com/in/bhavanadakshinamoorthy/"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="LinkedIn"
+            >
+              <img src={`${import.meta.env.BASE_URL}linkedin.jpg`} alt="LinkedIn" />
+            </a>
+
+            {/* Email link */}
+            <a
+              href="mailto:bdakshinamoorthy4@gmail.com"
+              aria-label="Mail"
+            >
+              <img src={`${import.meta.env.BASE_URL}mail.png`} alt="Email" />
+            </a>
+
+          </div>
           <div className="title-container">
             {/* Had to add an extra i so that the typing title worked */}
-            <TypingTitle text="Hii! I'm Bhavana" speed={90} /> 
+            <TypingTitle text="Hii! I'm Bhavana" speed={90} />
           </div>
           <Folder />
         </div>

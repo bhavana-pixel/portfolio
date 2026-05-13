8335c1884846e26ccf5231c0e4fc81a3980e6941
bhavana-pixel <bhavana0412@hotmail.com>
2026-05-13 12:33:59 -0400

    Changed states to vars.

--- src/Terminal.jsx
+++ src/Terminal.jsx
@@ -14,7 +14,7 @@ export default function TerminalLoader({ onFinish }) {
     "VITE v7.3.0  ready in 1077 ms"
   ];
 
-  /* States */
+  /* Vars */
   const [lines, setLines] = useState([]);
   const [currentText, setCurrentText] = useState("");
   const [stepIndex, setStepIndex] = useState(0);

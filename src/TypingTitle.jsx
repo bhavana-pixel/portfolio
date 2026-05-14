fc3d7f5a12e60b133721db2a3326623b9f597b98
bhavana-pixel <bhavana0412@hotmail.com>
2026-05-14 18:52:24 -0400

    Terminal function.

--- src/TypingTitle.jsx
+++ src/TypingTitle.jsx
@@ -6,7 +6,7 @@ import "./App.css";
 export default function TypingTitle({ text = "", speed = 100 }) {
   const [displayedText, setDisplayedText] = useState("");
 
-  // Function
+  // Terminal Function
   useEffect(() => {
     if (!text) return;
 

8bddaf70d1de77d96677f0f88d7a8a8675584e8c
bhavana-pixel <bhavana0412@hotmail.com>
2026-08-05 22:39:29 -0400

    Opening statement.

--- src/Folder.jsx
+++ src/Folder.jsx
@@ -2,7 +2,7 @@
 import { useState, useEffect } from "react";
 import "./Folder.css";
 
-{/* Function Opening */ }
+{/* Function Opening statement */ }
 export default function Folder() {
   const [activeTab, setActiveTab] = useState(null);
   const tabs = ["About", "Resume", "Projects", "Experience"];
